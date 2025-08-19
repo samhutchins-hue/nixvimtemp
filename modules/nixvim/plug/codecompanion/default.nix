02d571f75551c1393f218d3364b61e4f38c42451
samhutchins-hue <samhutchins123@gmail.com>
2025-08-18 21:56:35 -0700

    local ollama model for code companion

--- modules/nixvim/plug/codecompanion/default.nix
+++ modules/nixvim/plug/codecompanion/default.nix
@@ -1,5 +1,38 @@
 {
   plugins.codecompanion = {
     enable = true;
+
+    settings = {
+      adapters = {
+        ollama.__raw = ''
+          function()
+            return require("codecompanion.adapters").extend("ollama", {
+              env = {
+                url = "http://127.0.0.1:11434",
+              },
+              schema = {
+                model = {
+                  default = "qwen:32b",
+                },
+                num_ctx = {
+                  default = 32768,
+                },
+              },
+            })
+          end
+        '';
+      };
+      strategies = {
+        chat.adapter = "ollama";
+        inline.adapter = "ollama";
+        agent.adapter = "ollama";
+      };
+      opts = {
+        log_level = "TRACE";
+        send_code = true;
+        use_default_actions = true;
+        use_default_prompts = true;
+      };
+    };
   };
 }
